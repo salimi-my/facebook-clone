8c5cbaa1eef8d144120b7593692c3be47f16e19a
Mohamad Salimi <contact@salimi.my>
2022-01-05 02:36:17 +0800

    Update firebase config

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: 'AIzaSyBIRNp9xSE7czAk7k3tB9hRz74sQhZKZWs',
+  authDomain: 'facebook-clone-4a50b.firebaseapp.com',
+  projectId: 'facebook-clone-4a50b',
+  storageBucket: 'facebook-clone-4a50b.appspot.com',
+  messagingSenderId: '514143402885',
+  appId: '1:514143402885:web:b2daed199ea89ca3cde2ec'
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
