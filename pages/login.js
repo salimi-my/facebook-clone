121b885928d0f49a748966a3de928902c3e803e4
Mohamad Salimi <contact@salimi.my>
2022-01-09 18:58:19 +0800

    Add loading icon in login button

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,11 @@
 import { getProviders, signIn } from 'next-auth/react';
 import Head from 'next/head';
+import { useState } from 'react/cjs/react.development';
 import DisclaimerModal from '../components/modal/DisclaimerModal';
 
 function Login({ providers }) {
+  const [loggingIn, setLoggingIn] = useState(false);
+
   return (
     <>
       <Head>
@@ -56,9 +59,37 @@ function Login({ providers }) {
                     <button
                       className='w-full text-white border-0 py-2 px-8 focus:outline-none font-medium rounded-md text-xl 
                       bg-blue-600 hover:bg-blue-700'
-                      onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+                      onClick={() => {
+                        signIn(provider.id, { callbackUrl: '/' });
+                        setLoggingIn(true);
+                      }}
                     >
-                      Login with {provider.name}
+                      {loggingIn && (
+                        <div className='flex items-center justify-center'>
+                          <svg
+                            class='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                          >
+                            <circle
+                              class='opacity-25'
+                              cx='12'
+                              cy='12'
+                              r='10'
+                              stroke='currentColor'
+                              stroke-width='4'
+                            ></circle>
+                            <path
+                              class='opacity-75'
+                              fill='currentColor'
+                              d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                            ></path>
+                          </svg>
+                          <p className='font-semibold text-lg'>Connecting...</p>
+                        </div>
+                      )}
+                      {!loggingIn && <div>Login with {provider.name}</div>}
                     </button>
                   </div>
                 ))}
