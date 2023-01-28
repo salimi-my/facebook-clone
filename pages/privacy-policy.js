aac00cbbd3e32ef31b2e6113d9f73b626b20eb25
Mohamad Salimi <contact@salimi.my>
2023-01-29 00:52:08 +0800

    Fix apostrophe

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -209,11 +209,11 @@ function PrivacyPolicy() {
             Usage Data is collected automatically when using the Service.
           </p>
           <p className='mb-4'>
-            Usage Data may include information such as Your Device's Internet
-            Protocol address (e.g. IP address), browser type, browser version,
-            the pages of our Service that You visit, the time and date of Your
-            visit, the time spent on those pages, unique device identifiers and
-            other diagnostic data.
+            Usage Data may include information such as Your Device&apos;s
+            Internet Protocol address (e.g. IP address), browser type, browser
+            version, the pages of our Service that You visit, the time and date
+            of Your visit, the time spent on those pages, unique device
+            identifiers and other diagnostic data.
           </p>
           <p className='mb-4'>
             When You access the Service by or through a mobile device, We may
@@ -244,17 +244,17 @@ function PrivacyPolicy() {
           <p className='mb-4'>
             If You decide to register through or otherwise grant us access to a
             Third-Party Social Media Service, We may collect Personal data that
-            is already associated with Your Third-Party Social Media Service's
-            account, such as Your name, Your email address, Your activities or
-            Your contact list associated with that account.
+            is already associated with Your Third-Party Social Media
+            Service&apos;s account, such as Your name, Your email address, Your
+            activities or Your contact list associated with that account.
           </p>
           <p className='mb-4'>
             You may also have the option of sharing additional information with
-            the Company through Your Third-Party Social Media Service's account.
-            If You choose to provide such information and Personal Data, during
-            registration or otherwise, You are giving the Company permission to
-            use, share, and store it in a manner consistent with this Privacy
-            Policy.
+            the Company through Your Third-Party Social Media Service&apos;s
+            account. If You choose to provide such information and Personal
+            Data, during registration or otherwise, You are giving the Company
+            permission to use, share, and store it in a manner consistent with
+            this Privacy Policy.
           </p>
           <h3 className='mb-4 text-[24px] leading-[36px] font-bold'>
             Tracking Technologies and Cookies
@@ -377,11 +377,11 @@ function PrivacyPolicy() {
               <p className='mb-4'>
                 <strong>To contact You:</strong> To contact You by email,
                 telephone calls, SMS, or other equivalent forms of electronic
-                communication, such as a mobile application's push notifications
-                regarding updates or informative communications related to the
-                functionalities, products or contracted services, including the
-                security updates, when necessary or reasonable for their
-                implementation.
+                communication, such as a mobile application&apos;s push
+                notifications regarding updates or informative communications
+                related to the functionalities, products or contracted services,
+                including the security updates, when necessary or reasonable for
+                their implementation.
               </p>
             </li>
             <li className='mb-4'>
@@ -488,7 +488,7 @@ function PrivacyPolicy() {
           </h2>
           <p className='mb-4'>
             Your information, including Personal Data, is processed at the
-            Company's operating offices and in any other places where the
+            Company&apos;s operating offices and in any other places where the
             parties involved in the processing are located. It means that this
             information may be transferred to — and maintained on — computers
             located outside of Your state, province, country or other
@@ -582,7 +582,7 @@ function PrivacyPolicy() {
             cannot guarantee its absolute security.
           </p>
           <h1 className='mb-4 text-[40px] leading-[60px] font-bold'>
-            Children's Privacy
+            Children&apos;s Privacy
           </h1>
           <p className='mb-4'>
             Our Service does not address anyone under the age of 13. We do not
@@ -596,7 +596,7 @@ function PrivacyPolicy() {
           <p className='mb-4'>
             If We need to rely on consent as a legal basis for processing Your
             information and Your country requires consent from a parent, We may
-            require Your parent's consent before We collect and use that
+            require Your parent&apos;s consent before We collect and use that
             information.
           </p>
           <h1 className='mb-4 text-[40px] leading-[60px] font-bold'>
@@ -605,7 +605,7 @@ function PrivacyPolicy() {
           <p className='mb-4'>
             Our Service may contain links to other websites that are not
             operated by Us. If You click on a third party link, You will be
-            directed to that third party's site. We strongly advise You to
+            directed to that third party&apos;s site. We strongly advise You to
             review the Privacy Policy of every site You visit.
           </p>
           <p className='mb-4'>
